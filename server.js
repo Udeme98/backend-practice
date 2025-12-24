2de78af266ce37d2504b3294217d98e22dc6aaca
Udeme Udeme <darealudeme1@gmail.com>
2025-12-24 06:30:19 +0100

    authentication and authorization

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import bookRoutes from "./routes/bookRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use("/api/books", bookRoutes);
 
+app.use("/api/books", bookRoutes);
+app.use("/api/auth", authRoutes);
+
+// Connect to MongoDB
 connectDB();
 
 const PORT = process.env.PORT || 5001;
